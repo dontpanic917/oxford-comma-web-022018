1365a3e04a5f1972e6bd5afc1c5fe8e6f95e369a
Grayson Cowing <grayson.cowing@gmail.com>
2018-02-16 18:20:36 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,10 @@ def oxford_comma(array)
     return "#{array[0]} and #{array[1]}"
   elsif array.size == 3
     return "#{array[0]}, #{array[1]}, and #{array[2]}"
+  elsif array.size > 3
+    position=1
+    string1=""
+    array.each {|name| (position < array.size) ? string1 << "#{array[position-1]}, " : string1 << " and #{array[position-1]}" ; position += 1}
   end
 
 end
